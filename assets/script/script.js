40b68ebcc478f123584b79f802101563810b32fb
Diego Souza <diego-santos14@live.com>
2023-02-09 23:39:57 -0300

    Alterada função

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,7 +1,17 @@
+function img1(){
+	document.getElementByNameClass('basket').src('assets/img/carrinho2.png');
+}
+
+function img2(){
+	document.getElementByNameClass('basket').src('assets/img/carrinho.png');
+}
+
+
+
 const comprar = document.querySelector('.comprar');
 	comprar.addEventListener("click", function(){
 		const item = document.querySelector('.item');
-		const product = item[item, item, ...];
+		const product = item ["", "", ...];
 
 
 
