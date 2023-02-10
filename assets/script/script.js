3668a9561277fff982417312abfde6ea0d2839a2
Diego Souza <diego-santos14@live.com>
2023-02-09 22:52:46 -0300

    Criada função

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,2 +1,9 @@
 const comprar = document.querySelector('.comprar');
-	comprar.addEventListener("click", function();)
+	comprar.addEventListener("click", function(){
+		const item = document.querySelector('.item');
+		const product = item[item, item, ...];
+
+
+
+
+});
