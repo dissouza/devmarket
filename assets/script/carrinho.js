9fab17da02cb2d415a24fa8475c219d1055c74b7
Diego Souza <diego-santos14@live.com>
2023-02-27 19:14:09 -0300

    feat

--- assets/script/carrinho.js
+++ assets/script/carrinho.js
@@ -1,27 +1,5 @@
-let basket = localStorage.getItem('basket');
+let carrinho = JSON.parse(localStorage.getItem('carrinho')) || []; 
 
-if (basket) {
-    basket = JSON.parse(basket);
-
-    let itemExists = false;
-    for (let i = 0; i < basket.length; i++) {
-        if (basket[i].id === selectedItem.id) {
-            basket[i].qtd++;
-            itemExists = true;
-            break;
-        }
-    }
-
-    if (!itemExists) {
-        basket.push(selectedItem);
-        // Criar itens do carrinho aqui
-        // Pegar itens existentes e imprimir itens na página produtos
-        // Caso inexistentes, exibir "Não existem itens no carrinho"
-    }
-} else {
-    basket = [selectedItem];
-}
-
-localStorage.setItem('basket', JSON.stringify(basket));
+let conteudoCarrinho = document.getElementById("conteudo_carrinho");
 
 
